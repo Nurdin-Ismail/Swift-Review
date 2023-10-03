cc13f7cecfc7a3bdfb151409bbf009fdb5ce6446
FridaGathima <frida.gathima@student.moringaschool.com>
2023-10-03 15:08:25 +0300

    more validations added

--- server/models.py
+++ server/models.py
@@ -151,5 +151,7 @@ class ProductImg(db.Model, SerializerMixin):
     product_id = db.Column(db.Integer, db.ForeignKey('businesses.id'), nullable=False)
     productimgurl = db.Column(db.String)
 
+    
+
 
     
