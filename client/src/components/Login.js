15301f0c519099fd85c02e5a16f5902346e08c52
Gaitano <gaitano.george@student.moringaschool.com>
2023-10-03 12:03:45 +0300

    Added Login component

---
+++ client/src/components/Login.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+function Login(){
+
+    const [formInputs, setFormInputs] = useState({
+        username: "",
+        Password: ""
+    })
+
+    function handleChange(e){
+        setFormInputs(e.target.value)
+    }
+
+    function handleClick(e){
+        e.preventDefault();
+    }
+
+    return(
+        <div className="w-50 form_positioning">
+            <form className="form">
+                <div class="form-floating mb-3">
+                  <input onChange={handleChange} value={formInputs.username} type="text" class="form-control" id="floatingInput" placeholder="name@example.com"/>
+                  <label for="floatingInput">Username</label>
+                </div>
+                <div class="form-floating">
+                  <input onChange={handleChange} value={formInputs.Password} type="password" class="form-control" id="floatingPassword" placeholder="Password"/>
+                  <label for="floatingPassword">Password</label>
+                </div>
+                <button className="btn btn-primary frm_btn" onClick={handleClick} type="submit">LOGIN</button>
+            </form>
+        </div>
+    )
+}
+
+export default Login;
