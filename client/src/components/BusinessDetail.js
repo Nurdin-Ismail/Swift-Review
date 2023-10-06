bbea37995665e967e6a15ce9ae299f8728654e33
Gaitano <gaitano.george@student.moringaschool.com>
2023-10-06 10:21:38 +0300

    Completed the stylings

--- client/src/components/BusinessDetail.js
+++ client/src/components/BusinessDetail.js
@@ -4,108 +4,101 @@ import '../App.css';
 import ReviewDetail from './ReviewDetail';
 import axios from "axios";
 import ReviewForm from './ReviewForm';
+import Footer from './Footer';
 
 function BusinessDetail() {
-  const [business, setBusiness] = useState(null);
-  const [loading, setLoading] = useState(true);
-const params = useParams();
-const [reviews, setreviews] = useState([])
+    const [business, setBusiness] = useState(null);
+    const [loading, setLoading] = useState(true);
+  const params = useParams();
+  const [reviews, setreviews] = useState([])
 
 
-  // Define the handleReviewClick function to handle the review action
-  const handleReviewClick = (businessId) => {
-    // Implement your logic for handling reviews here
-    alert(`Review for business with ID ${businessId}`);
-  };
+    // Define the handleReviewClick function to handle the review action
+    const handleReviewClick = (businessId) => {
+      // Implement your logic for handling reviews here
+      alert(`Review for business with ID ${businessId}`);
+    };
 
-  useEffect(() => {
-    // Fetch data for the specific business using the businessId
-    const apiUrl = `http://127.0.0.1:5555/business/${params.id}`; // Replace with the actual URL
+    useEffect(() => {
+      // Fetch data for the specific business using the businessId
+      const apiUrl = `http://127.0.0.1:5555/business/${params.id}`; // Replace with the actual URL
 
-    axios
-      .get(apiUrl)
-      .then((response) => {
-        const data = response.data;
-        setBusiness(data);
-        setreviews(data.reviews)
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error('Error fetching data:', error);
-        setLoading(false);
-      });
-  }, [params]);
+      axios
+        .get(apiUrl)
+        .then((response) => {
+          const data = response.data;
+          setBusiness(data);
+          setreviews(data.reviews)
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.error('Error fetching data:', error);
+          setLoading(false);
+        });
+    }, [params]);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+    if (loading) {
+      return <div>Loading...</div>;
+    }
 
-  if (!business) {
-    return <div>Business not found.</div>;
-  }
+    if (!business) {
+      return <div>Business not found.</div>;
+    }
 
-  console.log(business);
+    console.log(business);
 
-  return (
-    <div>
-<section className="section"></section>
-<h1>{business.name}</h1>
-      <p>Category: {business.category}</p>
-      <p>Subcategory: {business.sub_category}</p>
-      <p>Contact: {business.contacts}</p>
-
-{/* Display Business Description */}
-      <div>
-        <h2>Description</h2>
-        <p>{business.description}</p>
-      </div>
-
-      <div>
-        <h2>Images</h2>
-        <img src="./images/download.jpeg" alt="Business Image" />
-        <img src="./images/mamanilishe.jpeg" alt="Business Image" />
-      </div>
-
-      {/* Display Business Hours */}
-      <div>
-        <h2>Business Hours</h2>
-        <ul>
-          <li>Monday: 9:00 AM - 6:00 PM</li>
-          <li>Tuesday: 9:00 AM - 6:00 PM</li>
-          <li>Wednesday: 9:00 AM - 6:00 PM</li>
-          <li>Thursday: 9:00 AM - 6:00 PM</li>
-          <li>Friday: 9:00 AM - 6:00 PM</li>
-        </ul>
-      </div>
-
-      <div>
-        <ReviewForm/>
-      </div>
-
-      {/* Display Ratings and Reviews */}
-      <div>
-        <h2>Ratings and Reviews</h2>
-        <div>
-          <h3>Overall Rating: 4.5/5</h3>
-          <p>Reviews:</p>
-          {reviews.map((review) => (
-            <ReviewDetail 
-            username={review.user.username}
-            rating= {review.rating}
-            comment={review.comment}
-            created_at={review.created_at}
-            
-            />
-          ))}
-        </div>
-      </div>
-
-            {/* Add a "Review" button */}
-      <div>
-        <button onClick={() => handleReviewClick(business.id)}>Review</button>
-      </div>
-    </div>
-  );
+    return (
+              <div>
+                <div className='bsns_dtls'>
+                  <h1  className="text-center text-uppercase fw-bolder text-decoration-underline">{business.name}</h1>
+                  <div className='bsns_dtls2'>
+                      <p>Category: {business.category}</p>
+                      <p>Subcategory: {business.sub_category}</p>
+                      <p>Contact: {business.contacts}</p>
+                      <div>
+                        <h2 className='text-uppercase'>Description</h2>
+                        <p>{business.description}</p>
+                      </div>
+                      <div>
+                        <h2 className='text-uppercase'>Images</h2>
+                        <img src="./images/download.jpeg" alt="Business Image" />
+                        <img src="./images/mamanilishe.jpeg" alt="Business Image" />
+                      </div>
+                      <div>
+                        <h2 className='text-uppercase'>Business Hours</h2>
+                        <ul>
+                          <li>Monday: 9:00 AM - 6:00 PM</li>
+                          <li>Tuesday: 9:00 AM - 6:00 PM</li>
+                          <li>Wednesday: 9:00 AM - 6:00 PM</li>
+                          <li>Thursday: 9:00 AM - 6:00 PM</li>
+                          <li>Friday: 9:00 AM - 6:00 PM</li>
+                        </ul>
+                          <h2 className='text-uppercase'>Ratings and Reviews</h2>
+                          <div>
+                            <h3 className='text-uppercase'>Overall Rating: 4.5/5</h3>
+                            <p>Reviews:</p>
+                          </div>
+                      </div>
+                  </div>
+                </div>
+                {/* Display Ratings and Reviews */}
+                  <div>
+                    <ReviewForm/>
+                  </div>
+                  <div>
+                    {reviews.map((review) => (
+                        <ReviewDetail 
+                        username={review.user.username}
+                        rating= {review.rating}
+                        comment={review.comment}
+                        created_at={review.created_at}
+                        
+                        />
+                      ))}
+                  </div>
+                  <Footer />
+              </div>
+    );
 }
 
 export default BusinessDetail;
