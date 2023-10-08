b5c15e7caf2a37e2e34aa2c851885cba997c5159
FridaGathima <frida.gathima@student.moringaschool.com>
2023-10-08 11:56:31 +0300

    user profile css

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -1,17 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import '../App.css'
-import { useParams } from 'react-router-dom';
-import BusinessDetail from './BusinessDetail';
-import ReviewDetail from './ReviewDetail'
+import React, { useEffect, useState } from "react";
+import "../App.css";
+import { useParams } from "react-router-dom";
+import BusinessDetail from "./BusinessDetail";
+import ReviewDetail from "./ReviewDetail";
 
 function UserProfile({ userId }) {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
-  const params = useParams()
-  console.log(params)
-  const [reviews, setreviews] = useState([])
-  
-  
+  const params = useParams();
+  console.log(params);
+  const [reviews, setreviews] = useState([]);
 
   // Define the handleReviewClick function to handle the review action
   // function handleReviewClick(userId){
@@ -27,12 +25,12 @@ function UserProfile({ userId }) {
       .then((response) => response.json())
       .then((data) => {
         setUser(data);
-        setreviews(data.reviews)
-        console.log(reviews)
+        setreviews(data.reviews);
+        console.log(reviews);
         setLoading(false);
       })
       .catch((error) => {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
         setLoading(false);
       });
   }, [params]);
@@ -46,34 +44,36 @@ function UserProfile({ userId }) {
   }
 
   return (
-      <div className='user-profile-main'>
-        <div className='user-profile-container'>
+    <div className="user-profile-main">
+      <section className="usersection">
+        <div className="user-profile-container">
           <h1>{user.username}</h1>
-          <p><b>Email:</b> {user.email}</p>
-          <p><b>Phone Number:</b> {user.contacts}</p>
-          <p><b>Membership Date:</b> {user.created_at}</p>
-          
-          {/* Display Ratings and Reviews */}
-          <p>Reviews:</p>
-          {reviews.map((review) => {
-                    return(
-                      <ReviewDetail
-                      username={'You'}
-                      rating = {review.rating}
-                      comment={review.comment}
-                      created_at={review.created_at}
-                      
-
-
-                      />
-
-
-                     )})}
-
-          {/* Display Business Description */}
-      
+          <p>
+            <b>Email:</b> {user.email}
+          </p>
+          <p>
+            <b>Phone Number:</b> {user.contacts}
+          </p>
+          <p>
+            <b>Membership Date:</b> {user.created_at}
+          </p>
         </div>
-      </div>
+        {/* Display Ratings and Reviews */}
+        <div className="reviews-dtls">
+          {reviews.map((review) => {
+            return (
+              <ReviewDetail
+                username={"You"}
+                rating={review.rating}
+                comment={review.comment}
+                created_at={review.created_at}
+              />
+            );
+          })}
+        </div>
+        {/* Display Business Description */}
+      </section>
+    </div>
   );
 }
 
